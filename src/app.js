34d2f2510081f717f66e81888c27e32ea34c13da
Pratham <pranka0789@gmail.com>
2025-10-11 13:46:41 +0530

    feat: set up Express server with MongoDB connection and utility classes
    - Added dependencies for Express, Mongoose, CORS, Cookie Parser, and dotenv.
    - Created an Express application with CORS and body parsing middleware.
    - Established a MongoDB connection using Mongoose.
    - Introduced ApiError and ApiResponse utility classes for standardized error and response handling.
    - Implemented asyncHandler utility for handling asynchronous route handlers.

---
+++ src/app.js
@@ -0,0 +1,26 @@
+// imports
+import express from "express"
+import cors from "cors"
+import cookieParser from "cookie-parser"
+
+
+const app = express()
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true
+}))
+
+app.use(express.json({limit: "16kb"}))
+app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.static("public"))
+app.use(cookieParser())
+
+// routes import
+// import route from "./"
+
+// routes declaration
+//app.use("/v1....", routeName)
+
+
+export {app}
