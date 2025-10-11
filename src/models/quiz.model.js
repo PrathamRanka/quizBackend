3ff827d707549284406481e82f41c9808bf19614
Sanchita Jain <sanchitajain1001@gmail.com>
2025-10-11 17:16:42 +0530

    made QuizSchema

---
+++ src/models/quiz.model.js
@@ -0,0 +1,13 @@
+// models/Quiz.js
+import mongoose, {Schema} from "mongoose";
+
+const QuizSchema = new Schema({
+  quizId: { type: String, required: true, unique: true },
+  title: { type: String, required: true },
+
+  durationSec: { type: Number, required: true }, // quiz duration in seconds
+  shuffleQuestions: { type: Boolean, default: True },
+  createdAt: { type: Date, default: Date.now }
+});
+
+export default mongoose.model("Quiz", QuizSchema);
