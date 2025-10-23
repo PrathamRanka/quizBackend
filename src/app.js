a232f5b4af09dd76a079aa99d7c46d8a6778cba3
Pratham <pranka0789@gmail.com>
2025-10-23 20:38:31 +0530

    removed status route from app.js

--- src/app.js
+++ src/app.js
@@ -38,7 +38,7 @@ app.use(cookieParser())
     import instructionsRoute  from "./routes/instructions.routes.js";
     import startQuizRoute from "./routes/startQuiz.route.js";
     import logoutRoute from "./routes/logout.routes.js";
-    import statusRoute from "./routes/status.routes.js";
+    // import statusRoute from "./routes/status.routes.js";
     import submitRoute from "./routes/submit.routes.js";
     import adminRoutes from "./routes/admin.routes.js";
     import exportDataRoutes from "./routes/exportData.routes.js";
@@ -48,7 +48,7 @@ app.use(cookieParser())
     app.use("/owasp-quiz/auth", loginRoute);
     app.use("/owasp-quiz/", instructionsRoute);
     app.use("/owasp-quiz/", startQuizRoute);
-    app.use("/owasp-quiz", statusRoute);
+    // app.use("/owasp-quiz", statusRoute);
     app.use("/owasp-quiz/", submitRoute);
     app.use("/owasp-quiz/auth", logoutRoute);
     app.use("/owasp-quiz/admin", adminRoutes);
